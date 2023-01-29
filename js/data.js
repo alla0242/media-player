d0a30509f08407bd36ffcfc4e4e03e414bf2e911
mat <alla0242@algonquinlive.com>
2023-01-29 13:24:34 -0500

    First Commit

---
+++ js/data.js
@@ -0,0 +1,52 @@
+const MEDIA = [
+  {
+    artist: 'Aesop Rock',
+    title: 'Shrunk',
+    track: 'aesop_rock_shrunk.mp3',
+    thumbnail: 'shrunk-small.png',
+    large: 'shrunk-large.png',
+  },
+    {
+    artist: 'Black Dahlia Murder',
+    title: 'What a Horrbile Night(To Have A Curse)',
+    track: 'black_dahlia_murder_what_a_horrible_night_to_have_a_curse.mp3',
+    thumbnail: 'black-small.png',
+    large: 'black-large.png',
+  },
+    {
+    artist: 'Dan Mangan',
+    title: 'In the Aeroplane Over the Sea',
+    track: 'dan_mangan_in_the_aeroplane_over_the_sea.mp3',
+    thumbnail: 'dan-small.png',
+    large: 'dan-large.png',
+  },
+    {
+    artist: 'Dead Kennedys',
+    title: 'Pull My Strings',
+    track: 'dead_kennedys_pull_my_strings.mp3',
+    thumbnail: 'dead-small.png',
+    large: 'dead-large.png',
+  },
+    {
+    artist: 'Guthrie Govan',
+    title: 'Ner Ner',
+    track: 'guthrie_govan_ner_ner.mp3',
+    thumbnail: 'guthrie-small.png',
+    large: 'nick-large.png',
+  },
+    {
+    artist: 'Nick Johnston',
+    title: 'Sandmonster',
+    track: 'nick_johnston_sandmonster.mp3',
+    thumbnail: 'nick-small.png',
+    large: 'nick-large.png',
+  },
+    {
+    artist: 'Danny Baranowsky',
+    title: 'Bloopa Doop Scooba',
+    track: 'the_basement_collection_blooba_doop_scooba.mp3',
+    thumbnail: 'basement-small.png',
+    large: 'basement-large.png',
+  },
+];
+export default MEDIA;
